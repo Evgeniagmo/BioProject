b789792e504ef4b20e2a0cbcf7a5e60feb61804b
Evgeniagmo <antoshkina.ep@phystech.edu>
2020-05-13 11:00:51 +0300

    calculating forces

--- node.cpp
+++ node.cpp
@@ -1,7 +1,13 @@
 #include "node.h"
 
-void Node::add_displacement(const t_point& displacement) noexcept
+void Node::add_displacement(const point_t& displacement) noexcept
 {
 	m_displacement.first += displacement.first;
 	m_displacement.second += displacement.second;
+}
+
+void Node::add_displacement(double x, double y) noexcept
+{
+	m_displacement.first += x;
+	m_displacement.second += y;
 }
--- node.h
+++ node.h
@@ -1,67 +1,49 @@
 #pragma once
 
-using t_point = std::pair <double, double>;
+#include <vector>
+
+using point_t = std::pair<double, double>;
 
 class Node
 {
 private:
 
-	t_point m_pos;
-	Link l_link, r_link;
-	t_point m_displacement; // for movement on each iteraction
+	point_t m_pos;
+
+	point_t m_displacement; // for movement on each iteraction
 
 public:
 
 	Node() noexcept = default;
 
-	//Node(const t_point& pos) : m_pos(pos)
-	//{
-	//	// и ребра задаются
-	//}
+	explicit Node(const point_t& pos) noexcept : m_pos(pos)
+	{}
 
 	explicit Node(double first, double second) noexcept :
-		m_pos(std::make_pair(first, second));
+		m_pos(std::make_pair(first, second))
 	{}
 
 	~Node() noexcept = default;
 
 public:
 
-	const t_point get_pos() const noexcept
+	const point_t get_pos() const noexcept
 	{
 		return m_pos;
 	}
 
-	void set_pos(const t_point& pos)
+	void set_pos(const point_t& pos)
 	{
 		m_pos = pos;
 	}
 
-	const Link get_left_link() const noexcept
-	{
-		return l_link;
-	}
-
-	void set_left_link(const Link& new_left)
-	{
-		l_link = new_left;
-	}
-
-	const Link get_right_link() const noexcept
-	{
-		return r_link;
-	}
-
-	void set_right_link(const Link& new_right)
-	{
-		r_link = new_right;
-	}
-
-	const t_point get_displacement() const noexcept
+	const point_t get_displacement() const noexcept
 	{
 		return m_displacement;
 	}
 
 	// displacement updates on each iteraction
-	void add_displacement(const t_point& displacement) noexcept;
-}
+	void add_displacement(const point_t& displacement) noexcept;
+
+	void add_displacement(double x, double y) noexcept;
+};
